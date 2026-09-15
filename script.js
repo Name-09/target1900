859ef64c21b76cdde4ee91b99dceb2c42e6fd4ee
Name-09 <mystmnk@gmail.com>
2026-09-15 12:00:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -337,7 +337,3 @@ function goHome() {
   document.getElementById("quiz").style.display = "none";
   document.getElementById("result").style.display = "none";
 }
-document.getElementById("quiz-title").innerText = quizTitle;
-
-  generateQuestion();   // ←これが絶対に必要
-}
